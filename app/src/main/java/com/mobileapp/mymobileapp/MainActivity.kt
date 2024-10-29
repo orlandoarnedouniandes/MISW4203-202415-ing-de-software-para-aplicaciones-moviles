12dc8fb1dd8cccea6717f2a803fcecaab8e78268
helverinio <h.wiesner@uniandes.edu.co>
2024-10-28 23:41:34 -0500

    implementación de hu03 - lista de artistas

--- app/src/main/java/com/mobileapp/mymobileapp/MainActivity.kt
+++ app/src/main/java/com/mobileapp/mymobileapp/MainActivity.kt
@@ -22,8 +22,7 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_albums
@@ -31,5 +30,9 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
