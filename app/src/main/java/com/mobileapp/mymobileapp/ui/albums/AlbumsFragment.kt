19d169b5c79da01169d00b57e1c872ea37da9253
Javier Estupiñan <j.estupinano@uniandes.edu.co>
2024-11-02 21:04:54 +0100

    Agregar test para la pantalla de albunes

--- app/src/main/java/com/mobileapp/mymobileapp/ui/albums/AlbumsFragment.kt
+++ app/src/main/java/com/mobileapp/mymobileapp/ui/albums/AlbumsFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
-import android.view.View.OnClickListener
 import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
