2248950ba1b7d7106766420c849e7333617275b3
orlandoarnedo <oarnedo@progi.com>
2024-11-23 13:31:17 -0500

    hu07 crear album, progress

--- app/src/main/java/com/mobileapp/mymobileapp/network/AlbumsApi.kt
+++ app/src/main/java/com/mobileapp/mymobileapp/network/AlbumsApi.kt
@@ -1,9 +1,16 @@
 package com.mobileapp.mymobileapp.network
 
 import com.mobileapp.mymobileapp.models.Album
+import com.mobileapp.mymobileapp.models.CreateAlbum
+import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface AlbumsApi {
     @GET("albums")
     suspend fun getAlbums(): List<Album>
+
+    @POST("/albums")
+    suspend fun createAlbum(@Body album: CreateAlbum): Response<CreateAlbum>
 }
