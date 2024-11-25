63be0846833011e926592cb80483371e30ce327c
Javier Estupiñan <j.estupinano@uniandes.edu.co>
2024-11-25 22:05:50 +0100

    Fix para album busqueda

--- app/src/androidTest/java/com/mobileapp/mymobileapp/AlbumsBusqueda.kt
+++ app/src/androidTest/java/com/mobileapp/mymobileapp/AlbumsBusqueda.kt
@@ -4,12 +4,12 @@ package com.mobileapp.mymobileapp
 import android.view.View
 import android.view.ViewGroup
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
 import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.action.ViewActions.scrollTo
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
+import androidx.test.espresso.matcher.ViewMatchers.withClassName
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withParent
 import androidx.test.espresso.matcher.ViewMatchers.withText
@@ -19,6 +19,7 @@ import androidx.test.filters.LargeTest
 import org.hamcrest.Description
 import org.hamcrest.Matcher
 import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.`is`
 import org.hamcrest.TypeSafeMatcher
 import org.junit.Rule
 import org.junit.Test
@@ -34,35 +35,19 @@ class AlbumsBusqueda {
 
     @Test
     fun albumsBusqueda() {
-        val bottomNavigationItemView = onView(
-            allOf(
-                withId(R.id.navigation_albums), withContentDescription("Albums"),
-                childAtPosition(
-                    childAtPosition(
-                        withId(R.id.nav_view),
-                        0
-                    ),
-                    0
-                ),
-                isDisplayed()
-            )
-        )
-        bottomNavigationItemView.perform(click())
-
         val appCompatEditText = onView(
             allOf(
                 withId(R.id.editTextText),
                 childAtPosition(
                     childAtPosition(
-                        withId(R.id.nav_host_fragment_activity_main),
+                        withClassName(`is`("android.widget.ScrollView")),
                         0
                     ),
                     1
-                ),
-                isDisplayed()
+                )
             )
         )
-        appCompatEditText.perform(replaceText("races"), closeSoftKeyboard())
+        appCompatEditText.perform(scrollTo(), replaceText("races"), closeSoftKeyboard())
 
         val textView = onView(
             allOf(
